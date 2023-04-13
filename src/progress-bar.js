96b87cfaf329e83e41e3110ea87e23ffedbc3a2d
noname-riss <122493434+noname-riss@users.noreply.github.com>
2023-04-13 12:51:13 -0400

    importing IntersectionObserver

--- src/progress-bar.js
+++ src/progress-bar.js
@@ -1,29 +1,46 @@
 import { LitElement, html, css } from 'lit';
+import { IntersectionObserverMixin } from "@lrnwebcomponents/intersection-element/lib/IntersectionObserverMixin.js";
+
 
 const logo = new URL('../assets/open-wc-logo.svg', import.meta.url).href;
 
-class Project3ProgressBar extends LitElement {
-  static properties = {
-    timer: { type: Number},
-    title: {type: String},
-
+class Project3ProgressBar extends IntersectionObserverMixin(LitElement) {
+  static get properties() {
+    let props = {};
+    if (super.properties) {
+      props = super.properties;
+    }
+    return{
+      startTime:{type: Number},
+      endTime:{type: Number},
+      title: {type: String}
+    }
   }
 
   static styles = css`
 
+  .wrapper
+  {
+    background-color: blue;
+    border: black;
+  }
   `;
 
   constructor() {
     super();
     this.title = 'Testing';
-    this.timer = 0;
+    this.startTime = 0;
+    this.endTime = 100;
   }
 
   render() {
     return html`
 
-    <div>
-    ${this.title}
+    <div class='wrapper'>
+      <div>
+      ${this.title}
+  </div>
+
     </div>
     `;
   }
